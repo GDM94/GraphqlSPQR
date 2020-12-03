1a37f041979bc487ecca0a4389d5bb7818e1a31d
GDM94 <giovanni.dimartino1994@gmail.com>
2020-12-03 15:03:54 +0100

    add log

--- src/main/java/com/example/springPostgres/SpringPostgresApplication.java
+++ src/main/java/com/example/springPostgres/SpringPostgresApplication.java
@@ -3,11 +3,17 @@ package com.example.springPostgres;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class SpringPostgresApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SpringPostgresApplication.class);
+
 	public static void main(String[] args) {
+		logger.debug("Hello from Logback");
 
 		SpringApplication.run(SpringPostgresApplication.class, args);
 	}
