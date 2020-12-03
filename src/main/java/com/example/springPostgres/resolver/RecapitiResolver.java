061252e00a171da748612f8e90133dd7c9f5843d
GDM94 <giovanni.dimartino1994@gmail.com>
2020-12-03 11:42:22 +0100

    add maven plugin

--- src/main/java/com/example/springPostgres/resolver/RecapitiResolver.java
+++ src/main/java/com/example/springPostgres/resolver/RecapitiResolver.java
@@ -1,20 +1,16 @@
 package com.example.springPostgres.resolver;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
-import com.example.springPostgres.model.Anagrafica;
+import com.example.springPostgres.IService.IAnagraficaService;
 import com.example.springPostgres.model.RecapitiTelefonici;
-import com.example.springPostgres.repositories.AnagraficaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RecapitiResolver implements GraphQLResolver<RecapitiTelefonici> {
-     private AnagraficaRepository anagraficaRepository;
 
-     public  RecapitiResolver(AnagraficaRepository anagraficaRepository){
-         this.anagraficaRepository = anagraficaRepository;
-     }
+     @Autowired
+     IAnagraficaService nagraficaService;
 
-     public Anagrafica getAnagrafica(RecapitiTelefonici recapitiTelefonici){
-          return anagraficaRepository.findById(recapitiTelefonici.getAnagrafica().getIdana()).orElse(null);
-     }
+
 }
