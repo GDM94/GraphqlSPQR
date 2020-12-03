d55f2ce0ab5fa7f5a1a050b8bb3752a3e55459e9
Gabe994 <luca.gabellini@outlook.it>
2020-12-03 10:55:37 +0100

    aggiunta nuovi package servizi

---
+++ src/main/java/com/example/springPostgres/IService/IAnagraficaService.java
@@ -0,0 +1,10 @@
+package com.example.springPostgres.IService;
+
+
+public interface IAnagraficaService {
+
+    public Anagrafica createAnagrafica(Anagrafica ana);
+    public void deleteAnagrafica(Long id);
+
+}
+
