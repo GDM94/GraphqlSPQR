0476f08a4859e4e626de3d3950342f915a0c4d21
Gabe994 <luca.gabellini@outlook.it>
2020-12-04 09:37:58 +0100

    progress on tests

--- src/test/java/com/example/springPostgres/repositories/AnagraficaRepositoryTest.java
+++ src/test/java/com/example/springPostgres/repositories/AnagraficaRepositoryTest.java
@@ -1,24 +1,16 @@
 package com.example.springPostgres.repositories;
 
 import com.example.springPostgres.model.Anagrafica;
-import javafx.beans.binding.BooleanExpression;
 import org.junit.ClassRule;
-
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
