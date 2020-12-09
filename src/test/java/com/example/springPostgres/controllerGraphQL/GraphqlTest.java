7db8651e75310a341015484e28ca07bd6bf120f5
GDM94 <giovanni.dimartino1994@gmail.com>
2020-12-09 09:53:44 +0100

    mokito

--- src/test/java/com/example/springPostgres/controllerGraphQL/GraphqlTest.java
+++ src/test/java/com/example/springPostgres/controllerGraphQL/GraphqlTest.java
@@ -1,10 +1,13 @@
 package com.example.springPostgres.controllerGraphQL;
-import org.json.JSONObject;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -24,7 +27,9 @@ public class GraphqlTest {
     @Test
     public void Test2() throws Exception {
         JSONObject jo = new JSONObject();
-        jo.put("query", "{anagraficaById(id: 2) { nome } }");
+        //jo.put("query", "{anagraficaById(id: 2) { nome } }");
+        jo.put("mutation", "{newAnagrafica(id: 10, nome:“test“, cognome: “test“) { nome } }");
+        System.out.println(jo.toString());
 
         this.mockMvc2.perform(MockMvcRequestBuilders
                 .post("/graphql")
