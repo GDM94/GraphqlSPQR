398d030bc1b4f7eac161dccbd4e2c0edb59efe99
GDM94 <giovanni.dimartino1994@gmail.com>
2020-12-10 16:15:54 +0100

    security config pt.1

--- src/main/java/com/example/springPostgres/security/JwtUserFactory.java
+++ src/main/java/com/example/springPostgres/security/JwtUserFactory.java
@@ -1,4 +1,32 @@
 package com.example.springPostgres.security;
 
-public class JwtUserFactory {
+import com.example.springPostgres.model.Authority;
+import com.example.springPostgres.model.User;
+import com.example.springPostgres.security.dto.JwtUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class JwtUserFactory {
+
+    private JwtUserFactory() {
+    }
+
+    public static JwtUser create(User user) {
+        return new JwtUser(
+                user.getUsername(),
+                user.getPassword(),
+                mapToGrantedAuthorities(user.getAuthorities()),
+                user.getEnabled()
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Authority> authorities) {
+        return authorities.stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName().name()))
+                .collect(Collectors.toList());
+    }
+
 }
