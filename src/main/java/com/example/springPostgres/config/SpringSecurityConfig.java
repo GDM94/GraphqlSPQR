81f4f19d17444abb47ed6503c98089c297937939
GDM94 <giovanni.dimartino1994@gmail.com>
2020-12-10 15:10:00 +0100

    basic

--- src/main/java/com/example/springPostgres/config/SpringSecurityConfig.java
+++ src/main/java/com/example/springPostgres/config/SpringSecurityConfig.java
@@ -4,17 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
+                .withUser("user").password("{noop}nttdata").roles("USER")
                 .and()
-                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+                .withUser("admin").password("{noop}nttdata").roles("USER", "ADMIN");
     }
 
     @Override
